2b42a49a57e7785d9c31a510d88376d96ddd92a0
Neurosyn Labs <neurosynlabs@proton.me>
2025-09-02 07:05:10 -0700

    Delete webchat directory

--- webchat/src/logic/formatting.js
+++
@@ -1,5 +0,0 @@
-// formatting.js - Simple response formatting guidelines for MARM
-
-export const RESPONSE_FORMATTING_RULES = `
-Response Format: Use clear paragraphs, bullet points for lists, **bold** for key terms. Write conversationally.
-`;
